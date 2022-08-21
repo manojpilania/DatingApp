bcd24a5e734b41c2a2118b6ab8b68383621d0d97
--global <--global>
2022-08-22 00:57:25 +0530

    End of section 10

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -14,9 +14,10 @@ namespace API.Helpers
         public AutoMapperProfiles()
         {
             CreateMap<AppUser, MemberDto>()
-            .ForMember(dest =>dest.PhotoUrl, opt=>opt.MapFrom(src=>src.Photos.FirstOrDefault(z=>z.IsMain).Url))
-            .ForMember(dest =>dest.Age, opt=>opt.MapFrom(src=>src.DateOfBirth.CalculateAge()));
+            .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(z => z.IsMain).Url))
+            .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoDto>();
+            CreateMap<MemberUpdateDto, AppUser>();
         }
     }
 }
